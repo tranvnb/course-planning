c07f49e7cdc773e7c6a58909962530d7c1cb585c
Brian Vo <vnbaotran@gmail.com>
2021-09-22 18:27:17 -0700

    reinstall prettier package

--- projects/course-planning/src/app/app.module.ts
+++ projects/course-planning/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { AvailableCoursesComponent } from './available-courses/available-courses.component';
+import { PlanningBoardComponent } from './planning-board/planning-board.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AvailableCoursesComponent,
+    PlanningBoardComponent,
+  ],
+  imports: [
+    MatCardModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
