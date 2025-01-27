09071d101a3e17832d8bcede91b71ffd91c48bc2
BrianVo <vnbaotran@gmail.com>
2025-01-26 16:32:23 -0800

    Update environment.prod.ts

--- projects/course-planning/src/environments/environment.prod.ts
+++ projects/course-planning/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://cobalt-rectangular-english.glitch.me',
+  process.env.API_URL: 'https://cobalt-rectangular-english.glitch.me',
 };
